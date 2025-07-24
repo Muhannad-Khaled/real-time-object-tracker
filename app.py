0b9d3ec505a4daf23129957c5d42009ca5fde866
Muhannad Khaled <119460687+Muhannad-Khaled@users.noreply.github.com>
2025-07-24 20:44:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,15 @@
-# import streamlit as st
-# import os
-# from tracker import run_tracker
-
-# st.set_page_config(page_title="Object Tracker", layout="centered")
-# st.title("🎯 Real-Time Object Tracker (From Video)")
-# st.write("Upload a video, choose a frame, and track a moving object.")
-
-# video_file = st.file_uploader("Upload a video file", type=["mp4", "avi", "mov"])
-
-# if video_file is not None:
-#     video_path = os.path.join("video.mp4")
-#     with open(video_path, "wb") as f:
-#         f.write(video_file.read())
-
-#     st.success("Video uploaded successfully!")
-
-#     if st.button("Start Object Tracking"):
-#         st.info("A window will pop up for you to select the object.")
-#         run_tracker(video_path)
-
 import streamlit as st
 import os
 from tracker import run_tracker_video, run_tracker_live
 
 st.set_page_config(page_title="Object Tracker", layout="centered")
-st.title("🎯 Real-Time Object Tracker")
+st.title("Real-Time Object Tracker")
 st.write("Track an object in a video or through your webcam.")
 
 # Tracking mode
-mode = st.radio("Choose tracking mode", ["📹 Upload a Video", "🎥 Live Camera"])
+mode = st.radio("Choose tracking mode", ["Upload a Video", "Live Camera"])
 
-if mode == "📹 Upload a Video":
+if mode == "Upload a Video":
     video_file = st.file_uploader("Upload a video", type=["mp4", "avi", "mov"])
 
     if video_file is not None:
